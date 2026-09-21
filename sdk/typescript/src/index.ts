95e1e5c8a0d8e7f77a6869bd15afcfea4af4b210
Vyncus Lim <ongyuze1401@gmail.com>
2026-09-21 13:06:13 +0800

    Export private semantic index

--- sdk/typescript/src/index.ts
+++ sdk/typescript/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./types.js";
 export * from "./crypto.js";
 export * from "./lexicon.js";
+export * from "./semantic-index.js";
 export * from "./negotiation.js";
 export * from "./runtime.js";
 export * from "./compiler.js";
